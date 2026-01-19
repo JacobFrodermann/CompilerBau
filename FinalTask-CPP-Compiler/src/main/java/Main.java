ac36ffd7a494b2de601995901b6adfb3d109bdb7
Client <client@clientcrash.com>
2026-01-19 22:55:36 +0100

    added reading test code from file

--- FinalTask-CPP-Compiler/src/main/java/Main.java
+++ FinalTask-CPP-Compiler/src/main/java/Main.java
@@ -1,15 +1,32 @@
+import ast.ASTPrinter;
+import ast.ProgramNode;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Main {
-  public static void main(String[] args) {
-    cppLexer lexer = new cppLexer(CharStreams.fromString("int main() {\nint i = 0;\n}\n"));
-    CommonTokenStream tokens = new CommonTokenStream(lexer);
-    cppParser parser = new cppParser(tokens);
+    public static void main(String[] args) {
+        try {
+            // Lesen aus file.cpp
+            cppLexer lexer = new cppLexer(CharStreams.fromPath(Paths.get("./src/test/resources/file.cpp")));
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            cppParser parser = new cppParser(tokens);
 
-    ParseTree tree = parser.start();
+            ParseTree tree = parser.start();
 
-    System.out.println(tree.toStringTree(parser));
-  }
+            // AST bauen
+            ASTBuilder builder = new ASTBuilder();
+            ProgramNode ast = builder.visitStart((cppParser.StartContext) tree);
+
+            // AST printen
+            ASTPrinter printer = new ASTPrinter();
+            System.out.println(printer.print(ast));
+
+        } catch (IOException e) {
+            System.err.println("Fehler beim Lesen der Datei: " + e.getMessage());
+        }
+    }
 }
---
+++ FinalTask-CPP-Compiler/src/test/resources/file.cpp
@@ -0,0 +1,15 @@
+int main(){
+
+}
+
+class Obj {
+    public:
+        int a;
+        Obj () {
+            a = "Test";
+        }
+        int test() {
+            return;
+        }
+        virtual int a(){}
+};
