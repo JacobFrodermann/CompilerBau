f65ee3936f7774787b7e856eee35f9afd0c78d38
Client <client@clientcrash.com>
2026-01-20 18:53:23 +0100

    Update file.cpp

--- FinalTask-CPP-Compiler/src/test/resources/file.cpp
+++ FinalTask-CPP-Compiler/src/test/resources/file.cpp
@@ -1,15 +1,62 @@
-int main(){
-
+int add(int a, int b) {
+    return a + b;
 }
 
-class Obj {
-    public:
-        int a;
-        Obj () {
-            a = "Test";
-        }
-        int test() {
-            return;
-        }
-        virtual int a(){}
+int add(int a, int b, int c) {
+    return a + b + c;
+}
+
+class Point {
+public:
+    int x;
+    int y;
+
+    Point() {
+        x = 0;
+        y = 0;
+    }
+
+    Point(int px, int py) {
+        x = px;
+        y = py;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    virtual int distance() {
+        return x + y;
+    }
 };
+
+class Point3D : public Point {
+public:
+    int z;
+
+    Point3D() {
+        z = 0;
+    }
+
+    int distance() {
+        return x + y + z;
+    }
+};
+
+int main() {
+    int result = add(5, 10);
+    int result2 = add(1, 2, 3);
+
+    Point p;
+    int px = p.getX();
+
+    if (result > 10) {
+        result = result - 5;
+    }
+
+    while (result > 0) {
+        result = result - 1;
+    }
+
+    return 0;
+}
